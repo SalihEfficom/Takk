41ea2ba837dc3de6c84e7c8821188477ac19f1ff
RayanHad <48243250+RayanHad@users.noreply.github.com>
2019-05-09 19:16:06 +0200

    Ajout interaction marqueur
    informations de la communauté quand on clique sur le marqueur et recherche par mot clé

--- Script/commu_carte.js
+++ Script/commu_carte.js
@@ -1,6 +1,19 @@
 var map;
 var mc;
 var markers=[];
+var marker;
+var markerCle;
+var newAddress;
+var infowindow;
+var content;
+var geoVille=[];
+var latlng;
+var nom=[];
+var desc=[];
+var mot=[];
+let data;
+var cpt=0;
+var geocoder ;
 
 function initMap() {
     var static_position = new google.maps.LatLng(46.9813764,2.60430911);
@@ -10,7 +23,28 @@ function initMap() {
         zoom: 6,
         draggable: true,
     });
+    infowindow = new google.maps.InfoWindow;
+    geocoder = new google.maps.Geocoder();
+
+    markerVille(geocoder, function (latlng, geoVille, nom, desc, mot) {
+        marker = new google.maps.Marker({
+            draggable: false,
+            raiseOnDrag: false,
+            position: latlng,
+            animation: google.maps.Animation.DROP,
+            ville: geoVille,
+            nom: nom,
+            desc: desc,
+            mot: mot,
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', onMarkerClick);
+        markers.push(marker);
+
+    });
 }
+
+
 
 function searchVille(){
     var inputVille = document.getElementById('input_ville');
@@ -21,7 +55,6 @@ function searchVille(){
 }
 
 function zoomVille(address) {
-    var geocoder = new google.maps.Geocoder();
     if (geocoder) {
         geocoder.geocode({ 'address': address }, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
@@ -31,55 +64,92 @@ function zoomVille(address) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+function markerVille(geocoder,myfunc) {
     var xhr = new XMLHttpRequest();
+
     xhr.open("GET", "../php/createMarker.php?", true);
     xhr.send();
 
     xhr.onreadystatechange = function() {
         let data = JSON.parse(xhr.response);
 
-        var geocoder = new google.maps.Geocoder();
-
-        for (i=0; i<data.length; i++) {
-            var newAddress;
-            geocoder.geocode( { 'address': data[i].ville }, function(results, status) {
+        for (let i=0; i<data.length; i++) {
+            nom[i] = data[i].nom;
+            desc[i] = data[i].description;
+            mot[i] = data[i].motcle;
+            geocoder.geocode( { 'address': data[i].ville}, function(results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
+                    console.log(i);
                     newAddress = results[0].geometry.location;
-                    var latlng = new google.maps.LatLng(parseFloat(newAddress.lat()),parseFloat(newAddress.lng()));
-                    markers.push(createMarker(latlng));
+                    latlng = new google.maps.LatLng(parseFloat(newAddress.lat()), parseFloat(newAddress.lng()));
+                    geoVille = results[0].address_components[0].short_name;
+
+                    myfunc(latlng, geoVille, nom[i], desc[i], mot[i]);
+                    // markers.push(createMarker(latlng, geoVille, nom[i], desc[i], mot[i]));
+
                 }
+                    // for (var x=0; x<9; x++) {
+                    //     markers.push(createMarker(latlng, geoVille, nom[x], desc[x], mot[x]));
+                    // }
+
             });
+
+
         }
-        mc = new MarkerClusterer(map, markers);
     }
-});
-
-function createMarker(latlng) {
-
-    var marker = new google.maps.Marker({
-        draggable: false,
-        raiseOnDrag: false,
-        position: latlng,
-        map: map
-    });
 }
 
-function createMarkerMotCle(latlng) {
+
+
+function createMarker(latlng,ville,nom,desc,mot) {
+    // console.log(nom);
+    // for (var x=0; x<9; x++) {
+        marker = new google.maps.Marker({
+            draggable: false,
+            raiseOnDrag: false,
+            position: latlng,
+            animation: google.maps.Animation.DROP,
+            ville: ville,
+            nom: nom,
+            desc: desc,
+            mot: mot,
+            map: map
+        });
+    // }
+}
+
+function onMarkerClick() {
+    var m = this;
+    console.log(m.nom);
+    infowindow.setContent( m.nom+"<br />"+m.ville+"<br />"+m.desc+"<br />"+m.mot);
+    infowindow.open(map, m);
+}
+
+function createMarkerMotCle(latlng,ville,nom,desc,mot) {
 
     var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|ffe436");
 
-    var marker = new google.maps.Marker({
+    markerCle = new google.maps.Marker({
         draggable: false,
         raiseOnDrag: false,
         icon: pinImage,
+        animation: google.maps.Animation.DROP,
         position: latlng,
+        ville: ville,
+        nom: nom,
+        desc: desc,
+        mot: mot,
         map: map
     });
+
+    markers.push(markerCle);
+
+    google.maps.event.addListener(markerCle, 'click', onMarkerClick);
+
 }
 
 function searchMotCle(){
-
+    setMapOnAll(null);
     var motcle = encodeURIComponent(document.getElementById("input_commu").value);
 
     var xhr = new XMLHttpRequest();
@@ -90,32 +160,32 @@ function searchMotCle(){
         let dataV = JSON.parse(xhr.response);
         var geocoder = new google.maps.Geocoder();
 
-        for (i=0; i<dataV.length; i++) {
+        for (let i=0; i<dataV.length; i++) {
+
+            nom[i] = dataV[i].nom;
+            desc[i] = dataV[i].description;
+            mot[i] = dataV[i].motcle;
             var newAddress;
             geocoder.geocode( { 'address': dataV[i].ville }, function(results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
                     newAddress = results[0].geometry.location;
                     var latlng = new google.maps.LatLng(parseFloat(newAddress.lat()),parseFloat(newAddress.lng()));
-                    markers.push(createMarkerMotCle(latlng));
+                    geoVille = results[0].address_components[0].short_name;
+                    createMarkerMotCle(latlng,geoVille,nom[i],desc[i],mot[i]);
                 }
             });
         }
-        mc = new MarkerClusterer(map, markers);
+        // mc = new MarkerClusterer(map, markers);
     }
 }
 
+function separateMotcle(value){
+        console.log('dqzdz');
+}
 
-//Creation d'une communauté
-function request() {
-    var xhr = getXMLHttpRequest();
-
-    var nom = encodeURIComponent(document.getElementById("nom").value);
-    var description = encodeURIComponent(document.getElementById("description").value);
-    var motcle = encodeURIComponent(document.getElementById("motcle").value);
-    var ville = encodeURIComponent(document.getElementById("ville").value);
-
-    xhr.open("GET", "../php/ajoutCommu.php?nom="+nom+"&description="+description+"&motcle="+motcle+"&ville="+ville, true);
-    xhr.send(null);
-
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
 }
 
