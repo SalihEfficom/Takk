69b2491665988ed41077f56f93d48b486f23a810
Salih Gungor <gungor.salih@outlook.fr>
2019-06-04 11:15:52 +0200

    gerer communautes wip

--- php/getInscriptions.php
+++ php/getInscriptions.php
@@ -6,23 +6,23 @@ include '../php/pdo/dbconfig.php';
 header("Content-Type: application/json");
 
 
-$getCreations = getCreations($_SESSION['mail'],$_SESSION['password']);
+$getInscription = getInscriptions($_SESSION['mail'],$_SESSION['password']);
 
 
-echo json_encode($getCreations);
+echo json_encode($getInscription);
 
 
 
-function getCreations($mail,$mdp){
+function getInscriptions($mail,$mdp){
     $data[] = array("success" => false,"data" => '');
-    if(isset($_SESSION['mail'],$_SESSION['password'])){
+    if(isset($mail,$mdp)){
         $mysqli = new mysqli("localhost", "root", "", "takk");
         $getInfoUser = getInfoMembre($_SESSION['mail'], $_SESSION['password']);
-        if ($result = $mysqli->query("SELECT * from communaute where admin=".$getInfoUser['id'])) {
+if ($result = $mysqli->query("SELECT c.nom,c.admin,mc.membre_id from communaute c inner join membre_communaute mc on c.id= mc.communaute_id inner join membre m on m.id=mc.membre_id where mc.membre_id =".$getInfoUser['id']." and c.admin !=".$getInfoUser['id'])) {
             $row = $result->fetch_all(PDO::FETCH_LAZY);
             $rowCounts = $result->num_rows;
             if($rowCounts == 0){
-                $data = array("success" => false,"data" =>"Vous n'avez actuellement aucune communautés de crées.");
+                $data = array("success" => false,"data" =>"Vous n'avez actuellement rejoin à aucune communautés de crées.");
             }else{
                 $data = array("success" => true,"data" =>$row);
             }
