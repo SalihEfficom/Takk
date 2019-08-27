18fb3ca61cdffac13a0e3cbcb84ae6aacd037cdb
Salih Gungor <gungor.salih@outlook.fr>
2019-08-27 09:29:54 +0200

    quitter une communauté

--- Script/commu.js
+++ Script/commu.js
@@ -1,42 +1,41 @@
+getCreation();
+getInscriptions();
 
-// function getMessage(){
-//     jQuery.ajax({
-//         url:"../php/getMessage.php",
-//         type:"GET",
-//         async:false,
-//         crossDomain:true,
-//         success:function(data){
-//             console.log("success ajax");
-//             console.log(data);
-//             document.getElementById("bloc1").innerHTML=data;
-//         },
-//         error:function(data){
-//             console.log("erreur ajax");
-//             console.log(data);
-//         }
-//     });
-// }
+function quit(identifier) {
+    // alert($(identifier).data('community'));
+    // alert($(identifier).data('user'));
+    var idUser = $(identifier).data('user');
+    var idCommunity = $(identifier).data('community')
+    if(confirm(`Êtes-vous sûre de vouloir quitter la communauté ?`))
+    {
+        $.ajax({
+            url:"../php/quitUserFromCommunity.php",
+            method: "POST",
+            data:{
+                idUser:idUser,
+                idCommunity:idCommunity
+            },
+            success:function(){
+                // $("div").find("[data-card='" + idCommunity + "']").css('background-color', '#ccc');
+                 $("div").find("[data-card='" + idCommunity + "']").fadeOut('slow');
+            }
+        });
+    }
+}
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    getCreation();
-    getInscriptions();
-});
+function getCreation() {
 
+    var xhr = getXMLHttpRequest();
 
-function getCreation(){
+    xhr.open("GET", "../php/getCreation.php?", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+            getCreation = JSON.parse(xhr.responseText);
+            if (getCreation.success) {
+                getCreation.data.forEach(function (element) {
+                    // console.log(element);
 
-  var xhr = getXMLHttpRequest();
-
-  xhr.open("GET", "../php/getCreation.php?", true);
-  xhr.onreadystatechange = function()
-  {
-      if(xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-        getCreation = JSON.parse(xhr.responseText);
-        if(getCreation.success){
-            getCreation.data.forEach(function(element) {
-                // console.log(element);
-
-                var content = `<div class="card blur-card">
+                    var content = `<div class="card blur-card">
                 <div class="card-header">
                     <div class="blur-img h-100 w-100"
                          style="background-image: url('../Content/img/mecano.jpg')"></div>
@@ -50,53 +49,50 @@ function getCreation(){
                     <button class="btn btn-link">GÉRER</button>
                 </div>`;
 
-                $( "#inner" ).append(content);
+                    $("#inner").append(content);
 
 
-              });
-        }else{
-            document.getElementById("falseMessage").innerHTML = getCreation.data;
+                });
+            } else {
+                document.getElementById("falseMessage").innerHTML = getCreation.data;
+            }
         }
-      }
-  }
-  xhr.send(null);
+    }
+    xhr.send(null);
 }
 
 
-function getInscriptions(){
+function getInscriptions() {
 
-  var xhr = getXMLHttpRequest();
+    var xhr = getXMLHttpRequest();
 
-  xhr.open("GET", "../php/getInscriptions.php?", true);
-
-  xhr.onreadystatechange = function()
-  {
-      if(xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-        getCreation = JSON.parse(xhr.responseText);
-        console.log(getCreation);
-        if(getCreation.success){
-            getCreation.data.forEach(function(element) {
-
-                var content = `<div class="card large-card">
+    xhr.open("GET", "../php/getInscriptions.php?", true);
+    var content = "";
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+            getCreation = JSON.parse(xhr.responseText);
+            if (getCreation.success) {
+                getCreation.data.forEach(function (element) {
+                    content += `<div data-card="${element.idCommunity}" class="card large-card">
                                     <div class="large-card-img"
                                         style="background-image: url('http://s1.lprs1.fr/images/2017/10/23/7350837_1854fc40-b819-11e7-ad33-44288a1b6cac-1_1000x625.jpg')"></div>
                                     <div class="large-card-content">
                                         <div class="card-title">${element.name}</div>
-                                        <button type="button" class="btn btn-outline-secondary">quitter</button>
+                                        <button onclick="quit(this)" id="delete" data-user="${element.idUser}" data-community="${element.idCommunity}" class="btn btn-outline-secondary">quitter</button>
                                     </div>
                                 </div>`;
 
-
-                $( "#innerInscription" ).append(content);
-
-            });
-        }else{
-            // document.getElementById("falseMessage").innerHTML = getCreation.data;
-            // alert('lol')
-           // alert('lol')
+                });
+                // var innerInscription = document.getElementById("innerInscription");
+                // innerInscription.innerHTML = content;
+                $("div#insert").replaceWith(content);
+            } else {
+                // document.getElementById("falseMessage").innerHTML = getCreation.data;
+                // alert('lol')
+                // alert('lol')
+            }
         }
-      }
-  }
-  xhr.send(null);
+    }
+    xhr.send(null);
 
 }
